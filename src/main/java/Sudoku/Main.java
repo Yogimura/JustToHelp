e2a55da26efab8628ff3084b044144192870f57d
Yogimura <137186279+Yogimura@users.noreply.github.com>
2024-07-20 19:44:41 -0400

    Group creations implementation, through ParseStrategy.java and ConstructorStrategy.java

--- src/main/java/Sudoku/Main.java
+++ src/main/java/Sudoku/Main.java
@@ -13,25 +13,24 @@ public class Main {
     private static final String[] numberSpell = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
     public static void main(String[] args) {
-        GroupFactory groupFactory = new GroupFactory();
-        String parseCode = "4,2:1,2,3,4,5,6,7,8,9:1,2,3,4,5,6,7,8,9";
-        Group gp2 = groupFactory.run(parseCode);
-        gp2.validate();
-        System.out.println(gp2);
-
-        Group gp3 = groupFactory.run(new baseGroup(6,3, List.of(values, values, values)));
-        gp3.validate();
-        System.out.println(gp3);
+        getGroups(2);
+        System.out.println();
+        getGroups(3);
+        System.out.println();
+        getGroups(4);
+        System.out.println();
+        getGroups(5);
     }
 
     private static void getGroups(Integer size) {
+        GroupFactory groupFactory = new GroupFactory();
         System.out.println("Groups of " + numberSpell[size-1]);
         List<List<Integer>> options = new ArrayList<>();
         for(int i = 0; i < size; i++) {
             options.add(values);
         }
         for(int i = 1; i <= 45; i++) {
-            Group gp = new Group(i, size, options);
+            Group gp = groupFactory.run(new baseGroup(i, size, options));
             gp.validate();
             if(gp.getValidValues().isEmpty()) {
                 continue;
